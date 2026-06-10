b015e45ee2b208c6b2a7964f2abb9bddedc8e4db
Ravi16Patidar <ravi4116patidar@gmail.com>
2026-06-10 13:22:48 +0530

    remove

--- index.cpp
+++ index.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-class Student{
-    private:
-    int age;
+// class Student{
+//     private:
+//     int age;
 
-    public :
-    void addData(int a){
-        age=a;
-    }
-    void displayData(){
-        cout<<"Age ="<<age;
-    }
-};
+//     public :
+//     void addData(int a){
+//         age=a;
+//     }
+//     void displayData(){
+//         cout<<"Age ="<<age;
+//     }
+// };
 
 // encapulation
 int main(){
